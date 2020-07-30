623ebdf66cf019c38dde4fdfd49115c84f938f6a
Jay <mgmediajay@gmail.com>
2020-07-30 21:49:42 +0500

    basic of laravel

---
+++ l-basic-web/resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('content')
+    
+<h1>Contacts</h1>
+@endsection
+@section('sidebar')
+    @parent
+        THis is really cool
+@endsection
---
+++ l-basic-web/resources/views/inc/sidebar.blade.php
@@ -0,0 +1,6 @@
+@section('sidebar')
+        <div class="sidebar">
+            <h3>Sidebar</h3>
+            This is side bar
+            @show
+        </div>
