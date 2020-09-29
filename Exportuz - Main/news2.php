bf7018a896d2c7ab2375b77e9297e74c3c52773a
Jay-Khakim <mgmediajay@gmail.com>
2020-09-29 13:12:17 +0500

    .

---
+++ Exportuz - Main/news2.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html  xml:lang="en-US" lang="en-US">
+<head>
+    <meta charset="utf-8">
+    <title>O'zbekiston eksportchilar uyushmasi</title>
+    <meta name="author" content="Mahmudjon Utkurov">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" type="text/css" href="stylesheets/bootstrap.css" >
+    <link rel="stylesheet" type="text/css" href="stylesheets/style.css">
+    <link rel="stylesheet" type="text/css" href="stylesheets/responsive.css">
+    <link rel="stylesheet" type="text/css" href="stylesheets/colors/color5.css" id="colors">
+    <link rel="stylesheet" type="text/css" href="stylesheets/animate.css">
+    <link href="icon/apple-touch-icon-48-precomposed.png" rel="apple-touch-icon-precomposed" sizes="48x48">
+    <link href="icon/apple-touch-icon-32-precomposed.png" rel="apple-touch-icon-precomposed">
+    <link href="icon/favicon.png" rel="shortcut icon">
+</head>                                 
+<body class="header-sticky page-loading">   
+    <div class="loading-overlay">
+    </div>
+    
+    <!-- Boxed -->
+    <div class="boxed">
+        <div class="top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="flat-address">  
+                            <div class="social-links">
+                                <a href="https://t.me/Eksportchilar_uyushmasi" target="_blank">
+                                    <i class="fa fa-paper-plane-o"></i>
+                                </a>
+                                <a href="https://www.facebook.com/uzex.uyushma" target="_blank">
+                                    <i class="fa fa-facebook"></i>
+                                </a>
+                                <a href="https://www.youtube.com/channel/UCqknB-bdjhBK10uZVyjuUnQ" target="_blank">
+                                    <i class="fa fa-youtube"></i>
+                                </a>
+                            </div>       
+                            <div class="custom-info">
+                                <span>Ijtimoiy tarmoqlar</span> 
+                                <i class="fa fa-reply"></i>info@exportuz.com 
+                                <i class="fa fa-phone"></i>(+998)71 207 00 98        
+                            </div>
+                        </div><!-- /.flat-address -->
+                    </div><!-- /.col-md-8 -->   
+                    <div class="col-md-4">
+                        <div class="top-navigator">        
+                            <ul>
+                                <li>
+                                    <a href="javascript:void(0)"><img src="images/icon/1.jpg" alt="JB's Language Icon"><span>En</span></a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)"><img src="images/icon/2.jpg" alt="JB's Language Icon"><span>Ru</span></a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)"><img src="images/icon/3.jpg" alt="JB's Language Icon"><span>Uz</span></a>
+                                </li>
+                            </ul>
+                        </div><!-- /.top-navigator -->
+                    </div><!-- /.col-md-4 -->              
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </div><!-- /.top -->
+
+        <?php
+            include 'header.php';
+        ?>
+
+        <!-- Page title -->
+        <div class="page-title style1">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">                    
+                        <div class="page-title-heading">
+                            <div class="breadcrumbs">
+                                <ul class="trail-items">
+                                    <li>Сиз ушбу манзилдасиз:</li>
+                                    <li class="trail-item"><a href="index.php">Асосий</a></li>
+                                    <li class="trail-item"><a href="news.php">Матбуот</a></li>
+                                    <li class="trail-end">Пресс релиз</li>
+                                </ul>                   
+                            </div>
+                        </div><!-- /.page-title-captions -->                        
+                    </div><!-- /.col-md-12 -->  
+                </div><!-- /.row -->  
+            </div><!-- /.container -->                      
+        </div><!-- /.page-title -->
+
+         <div class="flat-row ">
+            <div class="container">
+                <div class="row">
+                    <div class="flat-wrapper">
+                        <div class="general flat-awards-recognition">
+
+                            <div class="awards-recognition">
+                                <div >
+                                    <div class="ar-content">
+                                        <p><strong>Ўзбекистон тайёр кийим-кечак экспорти ҳажмини оширди</strong></p>
+                                        <p>Жорий йилнинг 1 сентябрь ҳолатига Ўзбекистон қиймати 336,7 миллион AҚШ долларига тенг бўлган тайёр кийим-кечак маҳсулотларини экспорт қилган. Бу йил 32 та давлатга тайёр кийим-кечак маҳсулотларини экспорт қилган Ўзбекистон ўтган йилнинг 8 ойига нисбатан экспорт ҳажмини 89,1 миллион AҚШ долларига оширган.</p>
+                                    </div>
+                                </div><!-- /.awards-recognition-item -->
+
+                                <div class="awards-recognition-item">
+                                    <div class="ar-content">
+                                        <p><strong>Ўзбекистон Тожикистонга маҳсулотлар экспорти ҳажмини оширди</strong></p>
+                                        <p>Жорий йилнинг 1 сентябрь ҳолатига Ўзбекистон билан Тожикистон ўртасидаги савдо айланмаси 317,0 млн. AҚШ долларини ташкил этган. Бунда экспорт улуши 255,1 млн. AҚШ доллари ва импорт ҳажми 61,9 млн. AҚШ долларига тенг. Давлат статистика қўмитаси маълумотларига кўра, саккиз ой мобайнида Тожикистонга амалга оширилган экспорт ҳажми ўтган йилнинг мос даврига солиштирилганда 65,1 млн. AҚШ долларига кўп.</p>
+                                    </div>
+                                </div><!-- /.awards-recognition-item -->
+
+                                <div class="awards-recognition-item">
+                                    <div class="ar-content">
+                                        <p><strong>Ўзбекистонга кайси хорижий давлатлардан инвестициялар киритилган?</strong></p>
+                                        <p>2019 йилда Ўзбекистон Республикасида асосий капиталга ўзлаштирилган инвестициялар ҳажми 195,9 трлн. сўмни ташкил этган. Ўсиш суръати 138,1 % га тенг.
+                                        Давлат статистика қўмитаси маълумотларига кўра, 2019 йилда асосий капиталга ўзлаштирилган жами хорижий инвестиция ва кредитлар таркибида хорижий инвесторлар улуши қуйидагича: Хитой – 26,2 %, Россия  – 10,6 %, Туркия – 5,2 %,Германия – 3,3 %, Швейцария – 2,9 %.</p>
+                                    </div>
+                                </div><!-- /.awards-recognition-item -->
+
+                                <div class="awards-recognition-item">
+                                    <div class="ar-content">
+                                        <p><strong>Ўзбекистон қайси давлатлардан ёғоч ва ёғоч маҳсулотларини импорт қилган?</strong></p>
+                                        <p>Жорий йилнинг 8 ойида Ўзбекистон хорижий давлатлардан қиймати 345,1 млн. АҚШ долларига тенг бўлган ёғоч ва ёғоч маҳсулотларини импорт қилган. Бу кўрсаткич ўтган йилнинг мос даври билан солиштирилганда 59,3 млн. АҚШ долларига кам.  Давлат статистика қўмитаси маълумотларига кўра, ўтган саккиз ой мобайнида импорт қилинган ёғоч ва ёғоч маҳсулотларининг энг катта улуши Россия Федерацияси (314,7 млн.), Хитой (10,8 млн.), Туркия (4,9 млн.), Белорусь (2,8 млн.), Италия (2,3 млн) ва 1,9 млн. АҚШ доллари билан Украина ҳиссасига тўғри келган.</p>
+                                    </div>
+                                </div><!-- /.awards-recognition-item -->
+                            </div><!-- /.awards-recognition -->
+                        </div><!-- /.company-history -->
+
+                        
+                    </div><!-- /.flat-wrapper -->
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </div><!-- /.flat-about -->
+
+       
+        <?php
+            include 'footer.php';
+        ?>
+    
+    <!-- Javascript -->
+    <script type="text/javascript" src="javascript/jquery.min.js"></script>
+    <script type="text/javascript" src="javascript/bootstrap.min.js"></script>
+    <script type="text/javascript" src="javascript/jquery.easing.js"></script> 
+    <script type="text/javascript" src="javascript/owl.carousel.js"></script> 
+    <script type="text/javascript" src="javascript/jquery-waypoints.js"></script>
+    <script type="text/javascript" src="javascript/jquery.fancybox.js"></script>
+    <script type="text/javascript" src="javascript/jquery.cookie.js"></script>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCIm1AxfRgiI_w36PonGqb_uNNMsVGndKo&v=3.7"></script>
+    <script type="text/javascript" src="javascript/gmap3.min.js"></script>
+    <script type="text/javascript" src="javascript/parallax.js"></script>
+    <!-- <script type="text/javascript" src="javascript/switcher.js"></script> -->
+    <script type="text/javascript" src="javascript/smoothscroll.js"></script>
+    <script type="text/javascript" src="javascript/jquery-validate.js"></script>
+    <script type="text/javascript" src="javascript/main.js"></script>
+
+    <!-- Revolution Slider -->
+    <script type="text/javascript" src="javascript/jquery.themepunch.tools.min.js"></script>
+    <script type="text/javascript" src="javascript/jquery.themepunch.revolution.min.js"></script>
+    <script type="text/javascript" src="javascript/slider.js"></script>
+</body>
+</html>
---
+++ Exportuz - Main/news3.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html  xml:lang="en-US" lang="en-US">
+<head>
+    <meta charset="utf-8">
+    <title>O'zbekiston eksportchilar uyushmasi</title>
+    <meta name="author" content="Mahmudjon Utkurov">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" type="text/css" href="stylesheets/bootstrap.css" >
+    <link rel="stylesheet" type="text/css" href="stylesheets/style.css">
+    <link rel="stylesheet" type="text/css" href="stylesheets/responsive.css">
+    <link rel="stylesheet" type="text/css" href="stylesheets/colors/color5.css" id="colors">
+    <link rel="stylesheet" type="text/css" href="stylesheets/animate.css">
+    <link href="icon/apple-touch-icon-48-precomposed.png" rel="apple-touch-icon-precomposed" sizes="48x48">
+    <link href="icon/apple-touch-icon-32-precomposed.png" rel="apple-touch-icon-precomposed">
+    <link href="icon/favicon.png" rel="shortcut icon">
+</head>                                 
+<body class="header-sticky page-loading">   
+    <div class="loading-overlay">
+    </div>
+    
+    <!-- Boxed -->
+    <div class="boxed">
+        <div class="top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="flat-address">  
+                            <div class="social-links">
+                                <a href="https://t.me/Eksportchilar_uyushmasi" target="_blank">
+                                    <i class="fa fa-paper-plane-o"></i>
+                                </a>
+                                <a href="https://www.facebook.com/uzex.uyushma" target="_blank">
+                                    <i class="fa fa-facebook"></i>
+                                </a>
+                                <a href="https://www.youtube.com/channel/UCqknB-bdjhBK10uZVyjuUnQ" target="_blank">
+                                    <i class="fa fa-youtube"></i>
+                                </a>
+                            </div>       
+                            <div class="custom-info">
+                                <span>Ижтимоий тармоқлар</span> 
+                                <i class="fa fa-reply"></i>info@exportuz.com 
+                                <i class="fa fa-phone"></i>(+998)71 207 00 98        
+                            </div>
+                        </div><!-- /.flat-address -->
+                    </div><!-- /.col-md-8 -->   
+                    <div class="col-md-4">
+                        <div class="top-navigator">        
+                            <ul>
+                                <li>
+                                    <a href="javascript:void(0)"><img src="images/icon/1.jpg" alt="JB's Language Icon"><span>En</span></a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)"><img src="images/icon/2.jpg" alt="JB's Language Icon"><span>Ru</span></a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)"><img src="images/icon/3.jpg" alt="JB's Language Icon"><span>Uz</span></a>
+                                </li>
+                            </ul>
+                        </div><!-- /.top-navigator -->
+                    </div><!-- /.col-md-4 -->              
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </div><!-- /.top -->
+
+        <?php
+            include 'header.php';
+        ?>
+
+        <!-- Page title -->
+        <div class="page-title style1">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">                    
+                        <div class="page-title-heading">
+                            <div class="breadcrumbs">
+                                <ul class="trail-items">
+                                    <li>Сиз ушбу манзилдасиз:</li>
+                                    <li class="trail-item"><a href="index.php">Асосий</a></li>
+                                    <li class="trail-item"><a href="about.php">Уюшма</a></li>
+                                    <li class="trail-end">Тадбирлар</li>
+                                </ul>                   
+                            </div>
+                        </div><!-- /.page-title-captions -->                        
+                    </div><!-- /.col-md-12 -->  
+                </div><!-- /.row -->  
+            </div><!-- /.container -->                      
+        </div><!-- /.page-title -->
+
+         <!-- Blog shortcode -->
+        <div class="flat-row">
+            <div class="container">
+                <div class="row">
+                    <div class="flat-wrapper">
+                        <div class="blog-shortcode clearfix">
+                            <article class="blog-posts item-three-column">
+                                <div class="entry-wrapper">
+                                    <div class="entry-cover">
+                                        <h4 class="entry-time">
+                                            <span class="entry-day">07</span>
+                                            <span class="entry-month">Cен</span>
+                                        </h4>
+                                        <a target="_blank" href="images/blog/vks1.jpg"><img src="images/blog/vks1.jpg" alt="images"></a>
+                                    </div>
+                                    <div class="entry-content-wrap">
+                                        <div class="entry-header">
+                                            <h2 class="entry-title">
+                                                <a target="_blank" href="images/blog/vks1.jpg">Малайзия Халқаро савдо ва саноат палатаси билан видео мулоқот</a>
+                                            </h2>
+                                        </div>
+                                        <div class="entry-footer">
+                                            <div class="entry-meta">
+                                                <span class="entry-author"><a href="#">ВКС</a></span>
+                                                <span class="entry-categories"><a href="#">ZOOM</a></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article><!-- /.blog-post -->
+
+                            <article class="blog-posts item-three-column">
+                                <div class="entry-wrapper">
+                                    <div class="entry-cover">
+                                        <h4 class="entry-time">
+                                            <span class="entry-day">20</span>
+                                            <span class="entry-month">Авг</span>
+                                        </h4>
+                                        <a target="_blank" href="images/blog/vks2.jpg"><img src="images/blog/vks2.jpg" alt="images"></a>
+                                    </div>
+                                    <div class="entry-content-wrap">
+                                        <div class="entry-header">
+                                            <h2 class="entry-title">
+                                                <a target="_blank" href="images/blog/vks2.jpg">Тўқимачилик ва енгил саноат уюшмаси билан видео мулоқат</a>
+                                            </h2>
+                                        </div>
+                                        
+                                        <div class="entry-footer"><br>
+                                            <div class="entry-meta">
+                                                <span class="entry-author"><a href="#">ВКС</a></span>
+                                                <span class="entry-categories"><a href="#">ZOOM</a></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article><!-- /.blog-post -->
+
+                            <article class="blog-posts item-three-column">
+                                <div class="entry-wrapper">
+                                    <div class="entry-cover">
+                                        <h4 class="entry-time">
+                                            <span class="entry-day">25</span>
+                                            <span class="entry-month">Авг</span>
+                                        </h4>
+                                        <a target="_blank" href="images/blog/vks3.jpg"><img src="images/blog/vks3.jpg" alt="images"></a>
+                                    </div>
+                                    <div class="entry-content-wrap">
+                                        <div class="entry-header">
+                                            <h2 class="entry-title">
+                                                <a target="_blank" href="images/blog/vks3.jpg">Саудиа Арабистон Подшохлиги билан видео мулоқат</a>
+                                            </h2>
+                                        </div>
+                                        
+                                        <div class="entry-footer"><br>
+                                            <div class="entry-meta">
+                                                <span class="entry-author"><a href="#">ВКС</a></span>
+                                                <span class="entry-categories"><a href="#">ZOOM</a></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article><!-- /.blog-post -->
+
+                            <article class="blog-posts item-three-column">
+                                <div class="entry-wrapper">
+                                    <div class="entry-cover">
+                                        <h4 class="entry-time">
+                                            <span class="entry-day">04</span>
+                                            <span class="entry-month">Cен</span>
+                                        </h4>
+                                        <a target="_blank" href="images/blog/vks4.jpg"><img src="images/blog/vks4.jpg" alt="images"></a>
+                                    </div>
+                                    <div class="entry-content-wrap">
+                                        <div class="entry-header">
+                                            <h2 class="entry-title">
+                                                <a target="_blank" href="images/blog/vks4.jpg">Австриа давлатидаги Ўзбекистон элчихонаси билан видео мулоқат</a>
+                                            </h2>
+                                        </div>
+                                        
+                                        <div class="entry-footer">
+                                            <div class="entry-meta">
+                                                <span class="entry-author"><a href="#">ВКС</a></span>
+                                                <span class="entry-categories"><a href="#">ZOOM</a></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article><!-- /.blog-post -->
+
+                            <article class="blog-posts item-three-column">
+                                <div class="entry-wrapper">
+                                    <div class="entry-cover">
+                                        <h4 class="entry-time">
+                                            <span class="entry-day">27</span>
+                                            <span class="entry-month">Авг</span>
+                                        </h4>
+                                        <a target="_blank" href="images/blog/vks5.jpg"><img src="images/blog/vks5.jpg" alt="images"></a>
+                                    </div>
+                                    <div class="entry-content-wrap">
+                                        <div class="entry-header">
+                                            <h2 class="entry-title">
+                                                <a target="_blank" href="images/blog/vks5.jpg">Австриа давлатидаги Ўзбекистон элчихонаси билан видео мулоқат</a>
+                                            </h2>
+                                        </div>
+                                        
+                                        <div class="entry-footer">
+                                            <div class="entry-meta">
+                                                <span class="entry-author"><a href="#">ВКС</a></span>
+                                                <span class="entry-categories"><a href="#">ZOOM</a></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article><!-- /.blog-post -->
+
+                            <article class="blog-posts item-three-column">
+                                <div class="entry-wrapper">
+                                    <div class="entry-cover">
+                                        <h4 class="entry-time">
+                                            <span class="entry-day">10</span>
+                                            <span class="entry-month">Авг</span>
+                                        </h4>
+                                        <a target="_blank" href="images/blog/vks6.jpg"><img src="images/blog/vks6.jpg" alt="images"></a>
+                                    </div>
+                                    <div class="entry-content-wrap">
+                                        <div class="entry-header">
+                                            <h2 class="entry-title">
+                                                <a target="_blank" href="images/blog/vks6.jpg">Ҳамкорлик ўрнатиш бўйича ишлар</a>
+                                            </h2>
+                                        </div>
+                                        
+                                        <div class="entry-footer"><br>
+                                            <div class="entry-meta">
+                                                <span class="entry-author"><a href="#">ВКС</a></span>
+                                                <span class="entry-categories"><a href="#">ZOOM</a></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article><!-- /.blog-post -->
+                        </div><!-- /.blog-shortcode -->
+                    </div><!-- /.flat-wrapper -->
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </div><!-- /.flat-about -->
+
+       
+        <?php
+            include 'footer.php';
+        ?>
+    
+    <!-- Javascript -->
+    <script type="text/javascript" src="javascript/jquery.min.js"></script>
+    <script type="text/javascript" src="javascript/bootstrap.min.js"></script>
+    <script type="text/javascript" src="javascript/jquery.easing.js"></script> 
+    <script type="text/javascript" src="javascript/owl.carousel.js"></script> 
+    <script type="text/javascript" src="javascript/jquery-waypoints.js"></script>
+    <script type="text/javascript" src="javascript/jquery.fancybox.js"></script>
+    <script type="text/javascript" src="javascript/jquery.cookie.js"></script>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCIm1AxfRgiI_w36PonGqb_uNNMsVGndKo&v=3.7"></script>
+    <script type="text/javascript" src="javascript/gmap3.min.js"></script>
+    <script type="text/javascript" src="javascript/parallax.js"></script>
+    <!-- <script type="text/javascript" src="javascript/switcher.js"></script> -->
+    <script type="text/javascript" src="javascript/smoothscroll.js"></script>
+    <script type="text/javascript" src="javascript/jquery-validate.js"></script>
+    <script type="text/javascript" src="javascript/main.js"></script>
+
+    <!-- Revolution Slider -->
+    <script type="text/javascript" src="javascript/jquery.themepunch.tools.min.js"></script>
+    <script type="text/javascript" src="javascript/jquery.themepunch.revolution.min.js"></script>
+    <script type="text/javascript" src="javascript/slider.js"></script>
+</body>
+</html>
